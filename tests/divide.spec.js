bf9dc927f6f96ab790f0e2da050c61a51e7baecf
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-06 12:31:10 +0200

    jasmine labs w1d5  Olga

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,22 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+    it("should return the division of the two numbers", () => {
+      expect(divide(10, 5)).toEqual(2);
+      expect(divide(5, 10)).toEqual(0.5);
+      expect(divide(200, 25)).toEqual(8);
+    });
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(5)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(undefined, 5)).toEqual(undefined);
+    });
+  });
+});
